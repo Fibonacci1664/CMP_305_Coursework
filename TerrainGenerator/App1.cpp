b418b3ab477cfd76d940d3298f5243f6fd47a92b
Fibonacci <dave.green1664@yahoo.co.uk>
2021-11-12 15:56:59 +0000

    First Commit
    Created a new starter project. Set up the dependencies and properties. The project runs showing a basic window.

---
+++ TerrainGenerator/App1.cpp
@@ -0,0 +1,100 @@
+#include "App1.h"
+#include <vector>
+App1::App1()
+{
+	
+}
+
+void App1::init(HINSTANCE hinstance, HWND hwnd, int screenWidth, int screenHeight, Input *in, bool VSYNC, bool FULL_SCREEN)
+{
+	// Call super/parent init function (required!)
+	BaseApplication::init(hinstance, hwnd, screenWidth, screenHeight, in, VSYNC, FULL_SCREEN);
+
+	// Load textures
+	textureMgr->loadTexture(L"brick", L"resources/textures/snow.png");
+	textureMgr->loadTexture(L"grass", L"resources/textures/grass.png");
+
+	// Create Mesh object and shader object
+
+
+	// Initialise light
+
+
+	//Initialise Camera Position
+	camera->setPosition(-16, 60, -15);
+	camera->setRotation(25, 45, 0);
+}
+
+
+App1::~App1()
+{
+	// Run base application deconstructor
+	BaseApplication::~BaseApplication();
+}
+
+
+bool App1::frame()
+{
+	bool result;
+
+	result = BaseApplication::frame();
+
+	if (!result)
+	{
+		return false;
+	}
+	
+	// Render the graphics.
+	result = render();
+
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool App1::render()
+{
+	XMMATRIX worldMatrix, viewMatrix, projectionMatrix;
+
+	// Clear the scene. (default blue colour)
+	renderer->beginScene(0.39f, 0.58f, 0.92f, 1.0f);
+
+	// Generate the view matrix based on the camera's position.
+	camera->update();
+
+	// Get the world, view, projection, and ortho matrices from the camera and Direct3D objects.
+	worldMatrix = renderer->getWorldMatrix();
+	viewMatrix = camera->getViewMatrix();
+	projectionMatrix = renderer->getProjectionMatrix();
+
+	// Send geometry data, set shader parameters, render object with shader
+	
+	// Render GUI
+	gui();
+
+	// Swap the buffers
+	renderer->endScene();
+
+	return true;
+}
+
+void App1::gui()
+{
+	// Force turn off unnecessary shader stages.
+	renderer->getDeviceContext()->GSSetShader(NULL, NULL, 0);
+	renderer->getDeviceContext()->HSSetShader(NULL, NULL, 0);
+	renderer->getDeviceContext()->DSSetShader(NULL, NULL, 0);
+
+	// Build UI
+	ImGui::Text("FPS: %.2f", timer->getFPS());
+	ImGui::Text("Camera Pos: (%.2f, %.2f, %.2f)", camera->getPosition().x, camera->getPosition().y, camera->getPosition().z);
+	ImGui::Checkbox("Wireframe mode", &wireframeToggle);
+
+	// Render UI
+	ImGui::Render();
+	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+}
+
